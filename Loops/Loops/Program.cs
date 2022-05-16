48f16396065112bde31922df0d2b09173c4cc14f
Selman Şener <selman.sener@oplog.com.tr>
2022-05-16 15:30:50 +0300

    Add Loops Example

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,109 @@
+﻿
+//for
+//while
+//do-while
+//foreach
+
+//break
+//continue
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("=====");
+Console.WriteLine();
+Console.WriteLine("=====");
+
+for (int i = 10; i > 0; i--)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("=====");
+Console.WriteLine();
+Console.WriteLine("=====");
+
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("=====");
+Console.WriteLine();
+Console.WriteLine("=====");
+
+int count = 0;
+while (count < 10)
+{
+    Console.WriteLine(count);
+    count++;
+}
+
+Console.WriteLine("=====");
+Console.WriteLine();
+Console.WriteLine("=====");
+
+count = 0;
+while (count != 0)
+{
+    Console.WriteLine(count);
+}
+
+Console.WriteLine("=====");
+Console.WriteLine();
+Console.WriteLine("=====");
+
+count = 0;
+do
+{
+    Console.WriteLine(count);
+    count++;
+} while (count != 0 && count < 5);
+
+Console.WriteLine("=====");
+Console.WriteLine();
+Console.WriteLine("=====");
+
+for (int i = 0; i < 10; i++)
+{
+    if (i > 5)
+    {
+        break;
+    }
+
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("=====");
+Console.WriteLine();
+Console.WriteLine("=====");
+
+for (int i = 0; i < 10; i++)
+{
+    if (i % 2 == 0)
+    {
+        continue;
+    }
+
+    Console.WriteLine(i);
+}
+
+// infinite loop
+/*
+for (int i = 1; i > 0; i++)
+{
+
+}
+
+while (true)
+{
+
+}
+
+do
+{
+
+} while (true);
+*/
